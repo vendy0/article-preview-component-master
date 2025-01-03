f18f7ae8abc6a4a7e1d1f09c898692189026367f
vendy0 <andyvenson99@gmail.com>
2025-01-03 17:36:36 -0500

    Style, Ok Js, Init

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 let button = document.querySelector("button");
 
-button.addEventListener("click", () => {
-  console;
-});
+function disparaitre() {
+  if (button.style.display == "inline" || button.style.display == "block") {
+    button.style.display = "none";
+  } else {
+    button.style.display = "inline";
+  }
+}
+
+// setInterval(disparaitre, 500);
+
+// q: how can I copy a function?
+// a: use the function name without the parentheses
+
+// q: how can I stop the setInterval?
+// a: use the clearInterval() function
+
+// How can I put the setInterval in a variable?
+// a: use the setInterval() function
+
+// how do we use the set interval function ?
+// a: setInterval(function, time)
